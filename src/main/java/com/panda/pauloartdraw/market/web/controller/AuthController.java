52469c335d66e6043d0ce2a0fb901a785c197749
Angie Valera <ingangiedvaleraa@gmail.com>
2022-02-07 07:43:46 -0500

    JWT is being generated

--- src/main/java/com/panda/pauloartdraw/market/web/controller/AuthController.java
+++ src/main/java/com/panda/pauloartdraw/market/web/controller/AuthController.java
@@ -6,6 +6,8 @@ import com.panda.pauloartdraw.market.domain.dto.ClientDto;
 import com.panda.pauloartdraw.market.domain.service.AuthService;
 import com.panda.pauloartdraw.market.domain.service.ClientService;
 import com.panda.pauloartdraw.market.web.security.JWTUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +25,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
 
+    private static Logger LOG = LoggerFactory.getLogger(AuthController.class);
+
     @Autowired
     private AuthenticationManager authenticationManager;
 
@@ -33,20 +37,17 @@ public class AuthController {
     private ClientService clientService;
 
     @Autowired
-    private BCryptPasswordEncoder encoder;
-
-    @Autowired
     private JWTUtil jwtUtil;
 
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponseDto> createToken(@RequestBody AuthenticationRequestDto request) {
         try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), encoder.encode(request.getPassword())));
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
             UserDetails userDetails = clientService.loadUserByUsername(request.getEmail());
             String jwt = jwtUtil.generateToken(userDetails);
             return new ResponseEntity<>(new AuthenticationResponseDto(jwt), HttpStatus.OK);
         } catch (BadCredentialsException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
         }
 
     }
--- src/main/java/com/panda/pauloartdraw/market/web/security/SecurityConfig.java
+++ src/main/java/com/panda/pauloartdraw/market/web/security/SecurityConfig.java
@@ -31,7 +31,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/**/login").permitAll()
+        http.cors().and().csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/**/login").permitAll()
+                .antMatchers("/**/register").permitAll()
                 .anyRequest().authenticated();
     }
 
