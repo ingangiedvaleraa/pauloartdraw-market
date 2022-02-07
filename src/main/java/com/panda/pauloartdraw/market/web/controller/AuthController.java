35b23a4b497d2c52ef425d4ecc7ed7b8dfe4d071
Angie Valera <ingangiedvaleraa@gmail.com>
2022-02-07 01:05:20 -0500

    Trying to set up jwt

--- src/main/java/com/panda/pauloartdraw/market/web/controller/AuthController.java
+++ src/main/java/com/panda/pauloartdraw/market/web/controller/AuthController.java
@@ -1,10 +1,19 @@
 package com.panda.pauloartdraw.market.web.controller;
 
+import com.panda.pauloartdraw.market.domain.dto.AuthenticationRequestDto;
+import com.panda.pauloartdraw.market.domain.dto.AuthenticationResponseDto;
 import com.panda.pauloartdraw.market.domain.dto.ClientDto;
 import com.panda.pauloartdraw.market.domain.service.AuthService;
+import com.panda.pauloartdraw.market.domain.service.ClientService;
+import com.panda.pauloartdraw.market.web.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +24,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
     private AuthService authService;
+
+    @Autowired
+    private ClientService clientService;
+
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponseDto> createToken(@RequestBody AuthenticationRequestDto request) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), encoder.encode(request.getPassword())));
+            UserDetails userDetails = clientService.loadUserByUsername(request.getEmail());
+            String jwt = jwtUtil.generateToken(userDetails);
+            return new ResponseEntity<>(new AuthenticationResponseDto(jwt), HttpStatus.OK);
+        } catch (BadCredentialsException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
 
     @PostMapping("/register")
     public ResponseEntity<ClientDto> save(@RequestBody ClientDto client) {
--- src/main/java/com/panda/pauloartdraw/market/web/security/SecurityConfig.java
+++ src/main/java/com/panda/pauloartdraw/market/web/security/SecurityConfig.java
@@ -1,17 +1,18 @@
 package com.panda.pauloartdraw.market.web.security;
 
-import com.panda.pauloartdraw.market.domain.dto.ClientDto;
-import com.panda.pauloartdraw.market.domain.repository.DomainClientRepository;
 import com.panda.pauloartdraw.market.domain.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -26,5 +27,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(clientService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers("/**/login").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
