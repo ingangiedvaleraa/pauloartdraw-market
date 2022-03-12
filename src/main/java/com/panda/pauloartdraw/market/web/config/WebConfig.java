95e5f27f94aa1e7f20a06eb453627af9354f3acf
Angie Valera <ingangiedvaleraa@gmail.com>
2022-03-12 15:08:21 -0500

    add methods to cors

--- src/main/java/com/panda/pauloartdraw/market/web/config/WebConfig.java
+++ src/main/java/com/panda/pauloartdraw/market/web/config/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedMethods("PUT", "DELETE", "POST", "GET")
                 .allowedOrigins("http://localhost:3005");
     }
 }
